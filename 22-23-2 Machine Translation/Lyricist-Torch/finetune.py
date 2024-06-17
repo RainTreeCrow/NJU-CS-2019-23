049504fe1f1e2f6f9fac78b2e5ab226eab1833c7
RainTreeCrow <1683762615@qq.com>
2024-06-17 15:34:44 +0100

    22-23-2 Machine Translation

---
+++ 22-23-2 Machine Translation/Lyricist-Torch/finetune.py
@@ -0,0 +1,23 @@
+import torch
+from data import Lyric
+from config import fix_length, output_size
+from utils import train
+
+
+learning_rate = 5e-4
+total_epoch = 101
+batch_size = 256
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+if __name__ == "__main__":
+    singers = ["邓丽君"]
+    for singer in singers:
+        print("=" * 10)
+        print("fine tune:" + singer)
+        dataset = Lyric(batch_size=batch_size,
+                        fix_length=fix_length,
+                        singer=singer,
+                        target_vocab_size=output_size,
+                        device=device)
+        train(dataset, learning_rate, total_epoch, device, save_epoch=total_epoch-1, log_step=5, check_epoch=5)
